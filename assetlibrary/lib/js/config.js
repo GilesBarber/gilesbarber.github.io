ca722b38d53316a0aaae7425f92674b9a7aa820f
GilesBarber <GilesBarber@gmail.com>
2020-02-01 00:20:55 +0000

    Asset Library
    Asset Library

---
+++ assetlibrary/lib/js/config.js
@@ -0,0 +1,14 @@
+var libNames = ['educational', 'orthodontic', 'academic'];
+var libraries = [];
+libraries[libNames[0]] = [];
+libraries[libNames[1]] = [];
+libraries[libNames[2]] = [];
+var className = ['educational', 'orthodontic', 'academic'];
+var IDprefix = ['EDU', 'ORTH', 'ACAD'];
+var labelColours = ['#6864E9', '#E72265', '#A4C532', '#F3BF27', '#189CD9']
+var daysMin = 1;
+var daysMax = 10;
+
+
+
+
