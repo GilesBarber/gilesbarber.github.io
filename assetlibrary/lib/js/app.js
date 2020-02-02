e046bafb57f23a3e44bf95319c00fdd346876566
GilesBarber <GilesBarber@gmail.com>
2020-02-02 20:27:37 +0000

    2ndJanUpdate
    2ndJanUpdate

--- assetlibrary/lib/js/app.js
+++ assetlibrary/lib/js/app.js
@@ -6,7 +6,7 @@ function populate(library, prefix, classes) {
 }
 
 function init() {
-  $(".multi-range").append('<input type="range" min="' + daysMin + '" max="' + daysMax + '" value="' + daysMin + '" id="lower" class="range-slider"> <input type="range" min="' + daysMin + '" max="' + daysMax + '" value="' + daysMax + '" id="upper" class="range-slider"></input>');
+  $(".multi-range").append('<span id="rs-bullet1" class="rs-label">' + daysMin + '</span><span id="rs-bullet2" class="rs-label">' + daysMax + '</span><input type="range" min="' + daysMin + '" max="' + daysMax + '" value="' + daysMin + '" id="lower" class="range-slider"> <input type="range" min="' + daysMin + '" max="' + daysMax + '" value="' + daysMax + '" id="upper" class="range-slider"></input>');
   var i;
   for (i = 0; i < libNames.length; i++) {
     initialiseLib(libNames[i], className[i], IDprefix[i]);
@@ -48,6 +48,7 @@ $(document).ready(function () {
   upperSlider.oninput = function () {
     lowerVal = parseInt(lowerSlider.value);
     upperVal = parseInt(upperSlider.value);
+    
 
     if (upperVal < lowerVal + 1) {
       $(".multiDay,#lowerValue").hide();
@@ -71,6 +72,8 @@ $(document).ready(function () {
   };
 
   $('input[type=range]').change(function () {
+    let rangeBullet1 = document.getElementById("rs-bullet1");
+    let rangeBullet2 = document.getElementById("rs-bullet2");
     var i;
     for (i = 0; i < libNames.length; i++) {
       $.each(libraries[libNames[i]], function (index, value) {
@@ -83,8 +86,38 @@ $(document).ready(function () {
         }
       });
     }
+   
+
+function showSliderValue() {
+  rangeBullet1.innerHTML = lowerVal;
+  rangeBullet2.innerHTML = upperVal;
+  var thing = $(".multi-range").width();
+  // console.log(rangeSlider.value);
+  let bulletPosition1 = ((100 / daysMax) * (lowerVal - 1) + (lowerVal/2));
+  rangeBullet1.style.left = bulletPosition1 + "%";
+  /*let bulletPosition1 =  ((lowerVal - 1) / daysMax) *
+    ($(".multi-range").width() + (lowerVal * 5));
+  rangeBullet1.style.left = bulletPosition1 + "px";*/
+
+  let bulletPosition2 = ((100 / daysMax) * (upperVal - 1) - (upperVal/2));
+  rangeBullet2.style.left = bulletPosition2 + "%";
+  /*
+  let bulletPosition2 =  ((upperVal - 1) / daysMax) *
+  ($(".multi-range").width() - (upperVal * 6));
+rangeBullet2.style.left = bulletPosition2 + "px";*/
+
+ // console.log(bulletPosition1,bulletPosition2,thing );
+
+}
+
     $("#lowerValue").text(lowerVal);
+    if(lowerVal==upperVal){
+      $("#rs-bullet2").hide()
+    }else{
+      $("#rs-bullet2").show();
     $("#upperValue").text(upperVal);
+    }
+    showSliderValue();
   });
 
   $('input[type=checkbox]').change(function () {
