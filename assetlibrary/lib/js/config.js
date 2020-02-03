b937b5d27c4c46ab5892b65853147f30a9a76a7b
GilesBarber <GilesBarber@gmail.com>
2020-02-03 19:03:58 +0000

    3rd Jan Passion Projects update.

--- assetlibrary/lib/js/config.js
+++ assetlibrary/lib/js/config.js
@@ -1,10 +1,11 @@
-var libNames = ['educational', 'orthodontic', 'academic'];
+var libNames = ['educational', 'orthodontic', 'academic','passionprojects'];
 var libraries = [];
 libraries[libNames[0]] = [];
 libraries[libNames[1]] = [];
 libraries[libNames[2]] = [];
-var className = ['educational', 'orthodontic', 'academic'];
-var IDprefix = ['EDU', 'ORTH', 'ACAD'];
+libraries[libNames[3]] = [];
+var className = ['educational', 'orthodontic', 'academic','passionprojects'];
+var IDprefix = ['EDU','ORTH','ACAD','PPRO'];
 var labelColours = ['#6864E9', '#E72265', '#A4C532', '#F3BF27', '#189CD9']
 var daysMin = 1;
 var daysMax = 10;
