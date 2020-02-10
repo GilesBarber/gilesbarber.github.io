13740f3261e63258a7fde4c8def5b12a95b65293
GilesBarber <34550461+GilesBarber@users.noreply.github.com>
2020-02-10 23:58:47 +0000

    FebUpdates

--- assetlibrary/lib/js/config.js
+++ assetlibrary/lib/js/config.js
@@ -1,11 +1,11 @@
-var libNames = ['educational', 'orthodontic', 'academic','passionprojects'];
+var libNames = ['educational', 'orthodontic', 'academic','personal'];
 var libraries = [];
 libraries[libNames[0]] = [];
 libraries[libNames[1]] = [];
 libraries[libNames[2]] = [];
 libraries[libNames[3]] = [];
-var className = ['educational', 'orthodontic', 'academic','passionprojects'];
-var IDprefix = ['EDU','ORTH','ACAD','PPRO'];
+var className = ['educational', 'orthodontic', 'academic','personal'];
+var IDprefix = ['EDU','ORTH','ACAD','PERS'];
 var labelColours = ['#6864E9', '#E72265', '#A4C532', '#F3BF27', '#189CD9']
 var daysMin = 1;
 var daysMax = 10;
